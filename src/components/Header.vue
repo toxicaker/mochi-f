aa4ad3b7fa5ab49d6ce427d92b390ba7afb43760
toxicaker <toxicaker@liangjiateng.cn>
2020-02-09 11:05:45 -0800

    :construction: Login
    Login

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,91 +1,103 @@
 <template>
-	<el-menu :default-active="activeIndex" mode="horizontal">
-		<el-menu-item class="brand" index="1">Mochi</el-menu-item>
-		<el-submenu index="2" style="float: right;">
-			<template slot="title"><i class="el-icon-user-solid"></i>Toxicaker</template>
-			<el-menu-item index="2-1">Profile</el-menu-item>
-			<el-menu-item index="2-2">Logout</el-menu-item>
-		</el-submenu>
-		<el-menu-item style="float: right;" @click="loginVisible = true">Login</el-menu-item>
-		<el-dialog title="Login" :visible.sync="loginVisible" width="30%">
-			<el-input id="username" v-model="username" style="margin-bottom: 10px;">
-				<template slot="prepend">Username</template>
-			</el-input>
-			<el-input id="password" v-model="password" type="password" style="margin-top: 10px;">
-				<template slot="prepend">Password</template>
-			</el-input>
-			<p style="margin-top: 20px;" align="right"><a href="#" style="margin-right: 20px;color: #409EFF;">Forget password?</a>
-				<a href="#" style="margin-right: 5px; color: #409EFF;">Sign up</a></p>
-			<span slot="footer" class="dialog-footer">
+    <el-menu :default-active="activeIndex" mode="horizontal">
+        <el-menu-item class="brand" index="1">Mochi</el-menu-item>
+        <el-submenu index="2" style="float: right;" v-if="isLoginStatus()">
+            <template slot="title"><i class="el-icon-user-solid"></i>Toxicaker</template>
+            <el-menu-item index="2-1">Profile</el-menu-item>
+            <el-menu-item index="2-2">Logout</el-menu-item>
+        </el-submenu>
+        <el-menu-item style="float: right;" @click="loginVisible = true" v-else>Login</el-menu-item>
+        <el-dialog title="Login" :visible.sync="loginVisible" width="30%">
+            <el-input id="username" v-model="username" style="margin-bottom: 10px;">
+                <template slot="prepend">Username</template>
+            </el-input>
+            <el-input id="password" v-model="password" type="password" style="margin-top: 10px;">
+                <template slot="prepend">Password</template>
+            </el-input>
+            <p style="margin-top: 20px;" align="right"><a href="#" style="margin-right: 20px;color: #409EFF;">Forget
+                password?</a>
+                <a href="#" style="margin-right: 5px; color: #409EFF;">Sign up</a></p>
+            <span slot="footer" class="dialog-footer">
 				<el-button @click="loginVisible = false">Cancel</el-button>
 				<el-button type="primary" @click="onLogin()" v-loading="loading">Login</el-button>
 			</span>
-		</el-dialog>
-	</el-menu>
+        </el-dialog>
+    </el-menu>
 </template>
 
 <script>
-	import axios from 'axios';
+    import axios from 'axios';
+    import {isLogin, setUserInfo} from '../utils';
 
-	export default {
-		name: 'Header',
-		data() {
-			return {
-				activeIndex: '1',
-				loginVisible: false,
-				username: '',
-				password: '',
-				loading: false
-			};
-		},
-		methods: {
-			onLogin: function() {
-				var myVue = this
-				this.loading = true
-				if (this.username === '' || this.password === '') {
-					this.$message.error('Username or password cannot be null');
-					this.loading = false
-					return
-				}
-				const payload = {
-					username: this.username,
-					password: this.password,
-				};
-				const url = this.apiHost + '/api/auth/sign-in'
-				axios.post(url, payload).then(function(response) {
-						if (response.data.code == 1) {
-							myVue.loginVisible = false
-							myVue.$message.success('Success!');
-							var token = response.data.data.token
-							
-						} else {
-							myVue.$message.error('Oops, ' + response.data.message);
-						}
-					}).catch(function(error) {
-						myVue.$message.error(error)
-					})
-					.then(function() {
-						myVue.loading = false
-					});
-			}
-		}
-	}
+    export default {
+        name: 'Header',
+        data() {
+            return {
+                activeIndex: '1',
+                loginVisible: false,
+                username: '',
+                password: '',
+                loading: false,
+                userInfo: {
+                    'userId': '',
+                    'username': '',
+                    'token': '',
+                },
+            };
+        },
+        methods: {
+            onLogin: function () {
+                let myVue = this;
+                this.loading = true;
+                if (this.username === '' || this.password === '') {
+                    this.$message.error('Username or password cannot be null');
+                    this.loading = false;
+                    return;
+                }
+                const payload = {
+                    username: this.username,
+                    password: this.password,
+                };
+                const url = this.apiHost + '/api/auth/sign-in';
+                axios.post(url, payload).then(function (response) {
+                    if (response.data.code === 1) {
+                        myVue.loginVisible = false;
+                        myVue.$message.success('Success!');
+                        myVue.userInfo.userId = response.data.data.id;
+                        myVue.userInfo.username = response.data.data.username;
+                        myVue.userInfo.token = response.data.data.token;
+                        setUserInfo(myVue.userInfo);
+                    } else {
+                        myVue.$message.error('Oops, ' + response.data.message);
+                    }
+                }).catch(function (error) {
+                    myVue.$message.error(error);
+                })
+                    .then(function () {
+                        myVue.loading = false;
+                    });
+            },
+            isLoginStatus: function () {
+                return isLogin()
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	.el-header {
-		line-height: 60px;
-		background-color: #409EFF;
-		box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
-	}
+    .el-header {
+        line-height: 60px;
+        background-color: #409EFF;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
+    }
 
-	.brand {
-		margin-right: 50px;
-		font-size: 20px;
-	}
+    .brand {
+        margin-right: 50px;
+        font-size: 20px;
+    }
 
-	.icon-active {
-		color: #409EFF
-	}
+    .icon-active {
+        color: #409EFF
+    }
 </style>
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,17 @@
-function isLogin(){
-	
+function isLogin() {
+    let token = window.localStorage.getItem("mochi-token");
+    return token !== '' && token != null;
 }
 
-function setToken(token){
-	
+function setUserInfo(userInfo) {
+    window.localStorage.setItem("mochi-userId", userInfo.userId);
+    window.localStorage.setItem("mochi-username", userInfo.username);
+    window.localStorage.setItem("mochi-token", userInfo.token);
 }
 
-function getToken(){
-	
+function getToken() {
+    return window.localStorage.getItem("mochi-token");
 }
+
+
+export {isLogin, setUserInfo, getToken}
